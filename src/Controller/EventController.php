adf829a67bd3a680fe12306b6ddabaa82625bacc
alucas2022 <antoine.lucas2022@campus-eni.fr>
2024-09-03 14:08:06 +0200

    Création eventController

---
+++ src/Controller/EventController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\EventRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/event', name: 'app_event')]
+
+class EventController extends AbstractController
+{
+    private $eventRepository;
+    private $cityRepository;
+    private $campusRepository;
+    private $placeRepository;
+    private $statusRepository;
+    private $userRepository;
+
+    /**
+     * @param $eventRepository
+     * @param $cityRepository
+     * @param $campusRepository
+     * @param $placeRepository
+     * @param $statusRepository
+     * @param $userRepository
+     */
+    public function __construct($eventRepository, $cityRepository, $campusRepository, $placeRepository, $statusRepository, $userRepository)
+    {
+        $this->eventRepository = $eventRepository;
+        $this->cityRepository = $cityRepository;
+        $this->campusRepository = $campusRepository;
+        $this->placeRepository = $placeRepository;
+        $this->statusRepository = $statusRepository;
+        $this->userRepository = $userRepository;
+    }
+
+
+    #[Route('/', name: '_list')]
+    public function index(): Response
+    {
+        return $this->render('event/index.html.twig', [
+            'controller_name' => 'EventController',
+        ]);
+    }
+}
