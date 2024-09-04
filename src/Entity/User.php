150e5a918d261f631113dd1f00713cd1b6a43fb5
naloulou2023 <aloulou.naycen@gmail.com>
2024-09-04 09:37:18 +0200

    modify message

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -12,8 +12,8 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
-#[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_NAME_SURNAME', fields: ['pseudo'])]
-#[UniqueEntity(fields: ['pseudo'], message: 'There is already an account with this name_surname')]
+#[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_PSEUDO', fields: ['pseudo'])]
+#[UniqueEntity(fields: ['pseudo'], message: 'There is already an account with this pseudo')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
